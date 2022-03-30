d0eefb485cbab39cd25e3f605cd8fa72e07fb6d5
David Cramer <dcramer@gmail.com>
2022-03-30 14:52:58 -0700

    Add some basic database stuff + prisma

---
+++ Makefile
@@ -0,0 +1,27 @@
+develop: install-requirements setup-git
+
+upgrade: install-requirements migrate-db
+
+setup-git:
+	pre-commit install
+	git config branch.autosetuprebase always
+
+install-requirements:
+	npm install
+
+test:
+	npm run test
+
+reset-db:
+	$(MAKE) drop-db
+	$(MAKE) create-db
+	$(MAKE) migrate-db
+
+drop-db:
+	dropdb --if-exists -h 127.0.0.1 -U postgres vanguard
+
+create-db:
+	createdb -E utf-8 -h 127.0.0.1 -U postgres vanguard
+
+migrate-db:
+	npx prisma migrate dev
---
+++ prisma/migrations/20220330215141_init/migration.sql
@@ -0,0 +1,39 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "content" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" INTEGER NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
