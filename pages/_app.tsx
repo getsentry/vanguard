bed88757291c6aab52d1b3be4d4a80ce583dfcab
David Cramer <dcramer@gmail.com>
2022-03-30 16:04:10 -0700

    Some initial boilerplate auth code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
 
-export default MyApp;
+export default App;
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,19 @@
+import { NextApiHandler } from "next";
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
+import prisma from "../../../lib/prisma";
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
+};
