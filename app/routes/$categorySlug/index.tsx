6aeee141a63643e8ae2ebfe0ef81c04245402ff5
David Cramer <dcramer@gmail.com>
2022-04-11 17:48:25 -0700

    Show category header

--- app/routes/$categorySlug/index.tsx
+++ app/routes/$categorySlug/index.tsx
@@ -17,6 +17,7 @@ export const loader: LoaderFunction = async ({ request, params }) => {
   const userId = await requireUserId(request);
   invariant(params.categorySlug, "categorySlug not found");
   const category = await getCategory({ slug: params.categorySlug });
+  invariant(category, "invalid category");
   const postList = await getPostList({
     userId,
     categoryId: params.categoryId,
@@ -26,27 +27,32 @@ export const loader: LoaderFunction = async ({ request, params }) => {
 };
 
 export default function Index() {
-  const data = useLoaderData() as LoaderData;
+  const { category, postList } = useLoaderData() as LoaderData;
 
-  return data.postList.length === 0 ? (
-    <p>No posts yet</p>
-  ) : (
-    <ol>
-      {data.postList.map((post) => (
-        <li key={post.id} className="post">
-          <h2>
-            <Link
-              to={`/${post.category.slug}/${post.id}-${slugify(post.title, {
-                lower: true,
-              })}`}
-            >
-              {post.title}
-            </Link>
-          </h2>
-          <h3>{post.author.name}</h3>
-          <p>{post.content}</p>
-        </li>
-      ))}
-    </ol>
+  return (
+    <div>
+      <h1>{category!.name}</h1>
+      {postList.length === 0 ? (
+        <p>No posts yet</p>
+      ) : (
+        <ol>
+          {postList.map((post) => (
+            <li key={post.id} className="post">
+              <h2>
+                <Link
+                  to={`/${post.category.slug}/${post.id}-${slugify(post.title, {
+                    lower: true,
+                  })}`}
+                >
+                  {post.title}
+                </Link>
+              </h2>
+              <h3>{post.author.name}</h3>
+              <p>{post.content}</p>
+            </li>
+          ))}
+        </ol>
+      )}
+    </div>
   );
 }
