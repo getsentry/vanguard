ee543992e57856303deda534484fe50b18552f07
David Cramer <dcramer@gmail.com>
2022-04-14 12:33:51 -0700

    Bring content styles in line

--- app/components/content.tsx
+++ app/components/content.tsx
@@ -1,20 +1,7 @@
 import styled from "styled-components";
 
 const Content = styled.div`
-  blockquote,
-  p,
-  pre,
-  ul,
-  ol {
-    margin: 30px 0;
-  }
-
   pre {
-    white-space: pre-wrap; /* Since CSS 2.1 */
-    white-space: -moz-pre-wrap; /* Mozilla, since 1999 */
-    white-space: -pre-wrap; /* Opera 4-6 */
-    white-space: -o-pre-wrap; /* Opera 7 */
-    word-wrap: break-word; /* Internet Explorer 5.5+ */
     background: #eee;
     padding: 20px;
   }
@@ -25,15 +12,10 @@ const Content = styled.div`
     border-left: 10px solid #eee;
   }
 
-  pre,
-  code {
-    font-family: monospace;
-  }
-
   img {
     max-width: 100%;
     display: block;
-    margin: 30px 0;
+    margin-bottom: 3rem;
   }
 `;
 export default Content;
---
+++ app/components/panel.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Block = styled.section`
+  margin: 30px 0;
+  border: 1px solid #ddd;
+  padding: 10px;
+`;
+
+export default Block;
