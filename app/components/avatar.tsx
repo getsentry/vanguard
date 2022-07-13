3888021e219a4e1f126a4f511a000d70f7796fd7
David Cramer <dcramer@gmail.com>
2022-07-13 15:30:18 -0700

    Add size

--- app/components/avatar.tsx
+++ app/components/avatar.tsx
@@ -1,15 +1,17 @@
 import type { User } from "@prisma/client";
 import styled from "styled-components";
 
-export default styled(({ user, ...props }: { user: User }) => {
-  return (
-    <img
-      src={user.picture || "/img/placeholder-avatar.png"}
-      alt="avatar"
-      {...props}
-    />
-  );
-})`
+export default styled(
+  ({ user, size, ...props }: { size?: string; user: User }) => {
+    return (
+      <img
+        src={user.picture || "/img/placeholder-avatar.png"}
+        alt="avatar"
+        {...props}
+      />
+    );
+  }
+)`
   display: block;
   width: ${(p) => (p.size ? p.size : "4.8rem")};
   height: ${(p) => (p.size ? p.size : "4.8rem")};
