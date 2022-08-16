3d7800ebdc11e5aee32e4c48003b6253664d2e90
David Cramer <dcramer@gmail.com>
2022-08-15 18:43:51 -0700

    ref: Add tests for reactions api route

---
+++ app/routes/api/posts/$postId/reactions.test.ts
@@ -0,0 +1,114 @@
+import type { Category, Post, User } from "@prisma/client";
+import { prisma } from "~/db.server";
+import { action } from "./reactions";
+
+const THUMBSUP = "👍";
+const HEART = "❤️";
+
+describe("post reactions action", () => {
+  let author: User;
+  let category: Category;
+  let post: Post;
+
+  beforeEach(async () => {
+    author = await prisma.user.create({
+      data: {
+        email: "foo@example.com",
+      },
+    });
+    category = await prisma.category.create({
+      data: {
+        name: "Foo Category",
+        slug: "foo-category",
+      },
+    });
+    post = await prisma.post.create({
+      data: {
+        title: "Test",
+        content: "**Content**",
+        deleted: false,
+        published: true,
+        authorId: author.id,
+        categoryId: category.id,
+      },
+    });
+  });
+
+  it("creates a new reaction", async () => {
+    const response: Response = await action({
+      request: new Request(`http://localhost/api/posts/${post.id}/reactions`, {
+        method: "POST",
+        body: JSON.stringify({ emoji: HEART }),
+      }),
+      params: { postId: post.id },
+      context: {},
+    });
+
+    expect(response.status).toBe(200);
+    const data = await response.json();
+    expect(data.emoji).toBe(HEART);
+    expect(data.delta).toBe(1);
+
+    const reactions = await prisma.postReaction.findMany();
+    expect(reactions.length).toEqual(1);
+    const reaction = reactions[0];
+    expect(reaction.postId).toBe(post.id);
+    expect(reaction.emoji).toBe(HEART);
+    expect(reaction.authorId).toBe(DefaultFixtures.DUMMY_USER.id);
+  });
+
+  it("deletes a new reaction that exists", async () => {
+    await prisma.postReaction.create({
+      data: {
+        emoji: HEART,
+        postId: post.id,
+        authorId: DefaultFixtures.DUMMY_USER.id,
+      },
+    });
+
+    const response: Response = await action({
+      request: new Request(`http://localhost/api/posts/${post.id}/reactions`, {
+        method: "POST",
+        body: JSON.stringify({ emoji: HEART }),
+      }),
+      params: { postId: post.id },
+      context: {},
+    });
+
+    expect(response.status).toBe(200);
+    const data = await response.json();
+    expect(data.emoji).toBe(HEART);
+    expect(data.delta).toBe(-1);
+
+    const reactions = await prisma.postReaction.findMany();
+    expect(reactions.length).toEqual(0);
+    const reaction = reactions[0];
+  });
+
+  it("does not delete differing emojis", async () => {
+    await prisma.postReaction.create({
+      data: {
+        emoji: HEART,
+        postId: post.id,
+        authorId: DefaultFixtures.DUMMY_USER.id,
+      },
+    });
+
+    const response: Response = await action({
+      request: new Request(`http://localhost/api/posts/${post.id}/reactions`, {
+        method: "POST",
+        body: JSON.stringify({ emoji: THUMBSUP }),
+      }),
+      params: { postId: post.id },
+      context: {},
+    });
+
+    expect(response.status).toBe(200);
+    const data = await response.json();
+    expect(data.emoji).toBe(THUMBSUP);
+    expect(data.delta).toBe(1);
+
+    const reactions = await prisma.postReaction.findMany();
+    expect(reactions.length).toEqual(2);
+  });
+});
--- test/setup-test-env.ts
+++ test/setup-test-env.ts
@@ -4,8 +4,28 @@ import { prisma } from "~/db.server";
 
 installGlobals();
 
+global.DefaultFixtures = {};
+
 const clearDatabase = async () => {
+  // TODO: good idea, but too slow
+  // const tablenames = await prisma.$queryRaw<
+  //   Array<{ tablename: string }>
+  // >`SELECT tablename FROM pg_tables WHERE schemaname='public'`;
+
+  // for (const { tablename } of tablenames) {
+  //   if (tablename !== "_prisma_migrations") {
+  //     try {
+  //       await prisma.$executeRawUnsafe(
+  //         `TRUNCATE TABLE "public"."${tablename}" CASCADE;`
+  //       );
+  //     } catch (error) {
+  //       console.log({ error });
+  //     }
+  //   }
+  // }
+
   await prisma.$transaction([
+    prisma.postReaction.deleteMany(),
     prisma.postMeta.deleteMany(),
     prisma.postRevision.deleteMany(),
     prisma.post.deleteMany(),
@@ -15,13 +35,29 @@ const clearDatabase = async () => {
   ]);
 };
 
-global.beforeAll(async () => {
+const createDummyUser = async () => {
+  return await prisma.user.create({
+    data: {
+      id: "cl6vih0pm16012nklaetl2tvze",
+      email: "jane.doe@example.com",
+      externalId: "dummy-iap-user",
+    },
+  });
+};
+
+// global.beforeAll(async () => {});
+
+global.beforeEach(async () => {
   await clearDatabase();
+
+  global.DefaultFixtures = {};
+
+  global.DefaultFixtures.DUMMY_USER = await createDummyUser();
 });
 
-global.afterEach(async () => {
-  await clearDatabase();
-});
+// global.afterEach(async () => {
+//   await clearDatabase();
+// });
 
 global.afterAll(async () => {
   await prisma.$disconnect();
