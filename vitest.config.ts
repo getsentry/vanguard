6e497ef16bf198710299a4a211943084da8b1409
David Cramer <dcramer@gmail.com>
2022-11-29 14:30:38 -0800

    More various fixes from bluestack

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,19 +1,18 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
-    threads: false,
     globals: true,
     environment: "happy-dom",
     setupFiles: ["./test/setup-test-env.ts"],
     include: ["./app/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    watchIgnore: [
+    watchExclude: [
       ".*\\/node_modules\\/.*",
       ".*\\/build\\/.*",
       ".*\\/postgres-data\\/.*",
