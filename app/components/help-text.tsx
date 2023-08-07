15c6103b4d3418eee85afe7710f19a7b12b132c8
David Cramer <dcramer@gmail.com>
2023-08-07 14:21:18 -0700

    Correct styles on HelpText

--- app/components/help-text.tsx
+++ app/components/help-text.tsx
@@ -1,5 +1,7 @@
 import type { ComponentPropsWithoutRef } from "react";
 
 export default function HelpText(props: ComponentPropsWithoutRef<"div">) {
-  return <div className="text-gray-800 text-xs" {...props} />;
+  return (
+    <div className="text-muted-light dark:text-muted-dark text-xs" {...props} />
+  );
 }
