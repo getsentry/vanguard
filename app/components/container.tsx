219d0bf52eeb7813ad58aa6e19e67ea6bff0e54e
Michi Hoffmann <cleptric@users.noreply.github.com>
2023-10-26 01:02:06 +0200

    Set container max width to `max-w-7xl` (1280px) (#124)

--- app/components/container.tsx
+++ app/components/container.tsx
@@ -1,5 +1,5 @@
 import type { ComponentPropsWithoutRef } from "react";
 
 export default function Container(props: ComponentPropsWithoutRef<"div">) {
-  return <div className="mx-auto xl:px-20 px-8 max-w-[120rem]" {...props} />;
+  return <div className="mx-auto xl:px-20 px-8 max-w-7xl" {...props} />;
 }
