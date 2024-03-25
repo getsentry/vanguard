6f28ade32a3f6729eb3da9a4cef85ec19cb829ad
David Cramer <dcramer@gmail.com>
2024-03-25 11:23:55 -0700

    Another attempted fix for dompurify

--- app/lib/summarize.test.ts
+++ app/lib/summarize.test.ts
@@ -10,4 +10,9 @@ describe("summarize", () => {
     const result = summarize("# foo bar\n\nhello world!", 8);
     expect(result).toBe("hello...");
   });
+
+  test("summarizes multi paragraph", async () => {
+    const result = summarize("**hello world**\n\nthis is a test\n\nfoo bar", 8);
+    expect(result).toBe("hello...");
+  });
 });
--- app/lib/summarize.ts
+++ app/lib/summarize.ts
@@ -4,16 +4,25 @@ import { sanitize } from "isomorphic-dompurify";
 export default (content: string, maxLength = 256): string => {
   // first remove elements we wouldn't want as a summary
   const contentBlocks = sanitize(marked.parse(content, { breaks: true }), {
-    ALLOWED_TAGS: ["p", "blockquote", "#text", "strong", "b", "em", "i", "a"],
+    ALLOWED_TAGS: [
+      "p",
+      "blockquote",
+      "#text",
+      "strong",
+      "b",
+      "em",
+      "i",
+      "a",
+      "#text",
+    ],
     KEEP_CONTENT: false,
   });
 
-  const sum = sanitize(contentBlocks, {
-    ALLOWED_TAGS: [],
-    KEEP_CONTENT: false,
-  })
-    .replace(/^\s*<p>(.*)<\/p>\s*$/gi, "$1")
-    .replace(/^[\s\n]+|[\s\n]+$/g, "");
+  const sum = (
+    sanitize(contentBlocks, {
+      RETURN_DOM: true,
+    }).textContent || ""
+  ).replace(/^[\s\n]+|[\s\n]+$/g, "");
   if (sum.length > maxLength)
     return sum.substring(0, maxLength - 3).split("\n")[0] + "...";
   return sum;
